b4f819b3a8ac980f928de1be7337a11abba499db
shabnam <shabnam@simplyphi.com>
2021-06-28 22:48:46 +0530

    connect state of store and call api to fetch data on app mount

--- src/App.js
+++ src/App.js
@@ -1,13 +1,40 @@
+import { Component } from "react";
+import { connect } from "react-redux";
+import { fetchProductList } from './actions'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h4 class="App-logo">ACME <i>Inc.</i></h4>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  componentDidMount() {
+    //On component mount call fetch request to load data
+    this.props.fetchProductList();
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h4 className="App-logo">ACME <i>Inc.</i></h4>
+        </header>
+        {this.props.isLoadingData ? (
+          <div>Loading . . .</div>
+        ) : this.props.error ? (
+          <div>Failed {this.props.error && this.props.error.message}</div>
+        ) : <div>Success</div>}
+      </div>
+    );
+  }
 }
 
-export default App;
+const mapStateToProps = ({ productList: { data = [], isLoadingData = false, error = null } }) => ({
+  data,
+  isLoadingData,
+  error
+});
+
+export default connect(
+  mapStateToProps,
+  {
+    fetchProductList
+  }
+)(App);
