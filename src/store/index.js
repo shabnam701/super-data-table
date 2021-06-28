72b7726b83c8477a9ccce97af2f9b7f081dcece5
shabnam <shabnam@simplyphi.com>
2021-06-28 22:47:04 +0530

    extend store capabilities with thunk and logger

--- src/store/index.js
+++ src/store/index.js
@@ -1,3 +1,13 @@
-import { createStore, applyMiddleware } from "redux";
-import thunk from "redux-thunk";
-const store = createStore(rootReducer, applyMiddleware(thunk));
+import { createStore, applyMiddleware, compose, combineReducers } from "redux";
+import logger from 'redux-logger'
+import thunk from 'redux-thunk'
+import * as rootReducer from '../reducers';
+import apiMiddleware from "../middleware/api";
+
+const initialState = {}
+const middlewareState = applyMiddleware(apiMiddleware, thunk, logger)
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+
+const store = createStore(combineReducers({ ...rootReducer }), initialState, composeEnhancers(middlewareState));
+window.store = store;
+export default store;
